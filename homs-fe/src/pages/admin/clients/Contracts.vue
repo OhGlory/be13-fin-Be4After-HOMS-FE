b6cd192dbda1ff40775f285af373e572af7e4a6b
psj1116 <psjprog1225@gmail.com>
2025-05-23 15:26:41 +0900

    feat : " dashboard 기능 구현"

--- homs-fe/src/pages/admin/clients/Contracts.vue
+++ homs-fe/src/pages/admin/clients/Contracts.vue
@@ -1,13 +1,122 @@
 <template>
     <div>
-        이곳은 계약관리 페이지 입니다
+        <!-- 제목 -->
+        <div class="text-3xl px-3 py-3">
+            <span>계약 목록</span>
+        </div>
+        <!-- 검색바 -->
+        <SearchBox @search="handleSearch" :selectOptions="handleSelectOption" :buttons="actionButtons"
+            :userRole="currentUserRole" />
+        <!-- 테이블 -->
+        <DynamicTable :columns="userColumns" :items="client" :showCheckbox="true">
+            <template #cell-id="{ item }">
+                <strong>{{ item.id }}</strong>
+            </template>
+            <template #cell-name="{ item }">
+                {{ item.name }}
+            </template>
+            <template #cell-email="{ item }">
+                <a :href="`mailto:${item.email}`">{{ item.email }}</a>
+            </template>
+            <template #actions="{ item }">
+                <button @click="detailClient(item)"
+                    class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded text-sm">
+                    조회
+                </button>
+            </template>
+        </DynamicTable>
+        <!-- 페이지 네비 -->
+        <PageNav :currentPage="currentPage" :totalPages="totalPages" @set-page="handleSetPage"></PageNav>
     </div>
 </template>
 
 <script setup>
+import SearchBox from '@/components/common/SaerchBar.vue';
+import DynamicTable from '@/components/common/DynamicTable.vue';
+import PageNav from '@/components/common/PageNav.vue';
+import { onMounted, ref } from 'vue';
+import apiClient from '@/api';
+
+
+const searchResult = ref(null);
+const currentPage = ref(1); // 현재 페이지 상태 관리
+const totalPages = ref(20); // 총 페이지 수 상태 관리
+const currentUserRole = ref('admin'); // 현재 유저 권한
+
+// ------- 검색바 --------
+const handleSearch = (searchData) => {
+  console.log('검색 데이터:', searchData);
+  // 여기서 검색 로직을 처리하거나 부모 컴포넌트로 데이터를 전달할 수 있습니다.
+  searchResult.value = searchData;
+};
+
+const handleSelectOption = ref([
+  { value: "", label: "전체" },
+  { value: "important", label: "중요" },
+  { value: "recent", label: "최근" },
+]);
+
+const actionButtons = ref([
+  {
+    label: "추가",
+    color: "bg-orange-500 hover:bg-orange-700",
+    action: (item) => console.log("추가:", item),
+    allowedRoles: ["admin", "editor"] // 이 버튼은 'admin' 또는 'editor'만 볼 수 있음
+  },
+  {
+    label: "일괄제거",
+    color: "bg-gray-500 hover:bg-gray-700",
+    action: (item) => console.log("삭제:", item),
+    allowedRoles: ["admin"] // 이 버튼은 'admin'만 볼 수 있음
+  },
+]);
+
+// ------- 테이블 --------
+const userColumns = ref([
+  { label: '순번', key: 'id' },
+  { label: '파트너사', key: 'companyName' },
+  { label: '계약품목', key: 'productName' },
+  { label: '계약기간', key: 'contract' },
+  { label: '담당부서', key: 'deptName' },
+  { label: '담당자', key: 'managerName' },
+  { label: '분류', key: 'categroy' },
+]);
+
+const client = ref([
+    { id: 1, companyName: '영광상사', productName: 'LDPE', contract: '2025-02-10 ~ 2026-02-10', deptName: '구매', managerName: '윤다희', categroy: 'PO'},
+]);
+
+const fetchData =  async () => {
+  const response =  await apiClient.get('admin/company')
+  const data = response.data.data;
+  console.log("거래처 데이터 받아오기", data);
+  client.value = data.map((item, index) => ({
+    id: item.companyId,
+    companyName: item.companyName,
+    productName: "LDPE",                          // 백엔드 수정 요청
+    contract: '2025-02-10 ~ 2026-02-10',          // 백엔드 수정 요청
+    deptName: '구매',                              // 백엔드 수정 요청
+    managerName: item.representManagerName,
+    categroy: 'PO'                                // 백엔드 수정 요청 
+  }));
+}
+
+const detailClient = (client) => {
+  console.log('거래처 상세조회:', client);  
+  console.log('거래처 id 정보', client.id);
+  console.log('거래처 회사명 조회', client.companyName);
+
+};
+
+// ------- 페이지네이션 --------
+const handleSetPage = (page) => {
+  console.log('페이지 변경 요청:', page);
+  currentPage.value = page;
+  // 여기서 해당 페이지의 데이터를 불러오는 로직 등을 수행해야 합니다.
+};
+
+onMounted (() => {
+  fetchData();
+})
 
 </script>
-
-<style lang="scss" scoped>
-
-</style>
