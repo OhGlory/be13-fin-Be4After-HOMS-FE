4ca5bbfcdc4bc6b03df0b6d215d374a55170a5f9
DESKTOP-CEKNQTC <jhss9747@gmail.com>
2025-06-01 23:43:00 +0900

    feat: "클레임 생성 구현"

--- homs-fe/src/pages/user/order/Order.vue
+++ homs-fe/src/pages/user/order/Order.vue
@@ -7,13 +7,18 @@
         <!-- 검색바 -->
         <SearchBox @search="handleSearch" :selectOptions="handleSelectOption" :userRole="authStore.isAdmin" />
         <!-- 테이블 -->
-        <DynamicTable :columns="orderColumns" :items="orders" :showCheckbox="false" :page="currentPage"
-            :pageSize="pageSize" @selected="handleSelectedItems" @row-click="handleRowClick" uniqueKey="orderId">
+        <DynamicTable
+            :columns="orderColumns"
+            :items="orders"
+            :showCheckbox="false"
+            :page="currentPage"
+            :pageSize="pageSize"
+            @selected="handleSelectedItems"
+            @row-click="handleRowClick"
+            uniqueKey="orderId"
+        >
             <!-- 항목 상세 설정 -->
-            <template #cell-id="{ item }">
-                <strong>{{ item.orderId }}</strong>
-            </template>
-            <template #cell-orderDate="{ item }">
+            <template #cell-orderDate="{item}">
                 {{ new Date(item.orderDate).toLocaleDateString() }}
             </template>
             <template #cell-dueDate="{item}">
